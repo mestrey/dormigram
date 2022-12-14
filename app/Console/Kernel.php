4a3b9409e8b37cbb457eef5bee747d292fe3c686
mestrey <yovan@ecovolve.fr>
2022-12-14 16:20:45 +0300

    Creating test and schedule command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule->command(AuthRemoveExpiredRefreshTokensCommand::class)->weekly();
     }
 }
